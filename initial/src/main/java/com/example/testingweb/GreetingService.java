a7887e17d78b67f99713ffbcb060b03e47e7e485
AjitGoel <AjitGoel@gmail.com>
2022-04-22 18:57:34 -0500

    4/22/2022

---
+++ initial/src/main/java/com/example/testingweb/GreetingService.java
@@ -0,0 +1,8 @@
+package com.example.testingweb;
+import org.springframework.stereotype.Service;
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
+}
